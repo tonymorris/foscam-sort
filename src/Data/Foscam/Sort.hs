c400d0774f727bb35a4c3845c14aed53f73db453
Tony Morris <tmorris@tmorris.net>
2015-12-02 11:50:27 +1000

    sort

--- src/Data/Foscam/Sort.hs
+++ src/Data/Foscam/Sort.hs
@@ -1,50 +1,64 @@
-module Data.Foscam.Sort where
+module Data.Foscam.Sort(
+  foscamsort
+) where
 
-import Data.Monoid
-import Control.Monad
-import Data.Foscam.File -- (Filename(..), AsDate(..), AsTime(..))
-import Data.Foscam.Sort.FilenamePath
-import Data.Foscam.Directory
-import Prelude
-import Control.Lens
-import System.Directory
-import System.FilePath
-import System.Posix.Files
+import Control.Category(id, (.))
+import Control.Lens((^.), (^?), _2)
+import Control.Monad((>>=), when, unless)
+import Data.Bool(Bool(True))
+import Data.Either(either)
+import Data.Foldable(Foldable, mapM_)
+import Data.Foscam.Directory(getFoscamDirectoryContents, FoscamDirectoryFile, _FoscamDirectory, _FoscamFilename, _Is)
+import Data.Foscam.File(getFilename)
+import Data.Foscam.Sort.FilenamePath(FilenamePath, yyyy, mm, dd, nn, (/<>/), (~/>))
+import Data.Maybe(maybe)
+import Data.Monoid((<>))
+import System.Directory(doesFileExist, getPermissions, readable, renameFile, doesFileExist, createDirectoryIfMissing)
+import System.Environment(getArgs, getProgName)
+import System.FilePath(FilePath, (</>))
+import System.IO(IO, hPutStrLn, stderr)
+import System.Posix.Files(createSymbolicLink)
 
-main ::
+foscamsort ::
   IO ()
-main = 
-  let p = [yyyy <> mm <> dd, yyyy /<>/ mm, yyyy /<>/ mm /<>/ dd] -- , yyyymm, yyyymmdd, yyyymmddhh, yyyymmddhhmm]
-      q = join [(deviceIdpath /<>/) <$> p, (aliaspath /<>/) <$> p, p]
-  in run q "/home/tmorris/Desktop/test/files" "/home/tmorris/Desktop/test/out"
-  
-pr = 
-  getFoscamDirectoryContents "/home/tmorris/Desktop/test/files" >>=
-  mapM_ move
+foscamsort = 
+  do a <- getArgs
+     case a of
+       (i:o:_) ->
+         getFoscamDirectoryContents i >>=
+         mapM_ (move o (yyyy <> mm <> dd) [yyyy, yyyy /<>/ mm, yyyy /<>/ mm /<>/ dd, yyyy /<>/ mm /<>/ dd /<>/ nn]) 
+       _ ->
+         do p <- getProgName
+            hPutStrLn stderr ("Usage: " <> p <> " <foscam-files-directory> <sort-output-directory>")
 
-move q =
-  let (name, w) = case q of
-                    Isn't _ n -> (n, "unknown")
-                    Is    _ n -> (getFilename n, "date" </> (yyyy <> mm <> dd) ~/> n)
-      d = "/home/tmorris/Desktop/test/move/" </> w
-      a1 = q ^. _FoscamDirectory </> name
-      a2 = d </> name
-  in do createDirectoryIfMissing True d
-        putStrLn ("Moving " ++ a1 ++ " to " ++ a2)
-        copyFileIf a1 a2 -- todo move
-        createDirectoryIfMissing True "/home/tmorris/Desktop/test/link/"
-        createSymbolicLinkIfMissing
-          "../move/date/20150205/00626E44C831(house)_1_20150205220252_23195.jpg"
-          "/home/tmorris/Desktop/test/link/xyz"
- 
-copyFileIf ::
+move ::
+  Foldable t =>
+  FilePath
+  -> FilenamePath
+  -> t FilenamePath
+  -> FoscamDirectoryFile
+  -> IO ()
+move base mvpath lnpaths q =
+  let fname = q ^? _Is . _2
+      name = either id getFilename (q ^. _FoscamFilename)
+      mv = base </> "move" </> maybe "unknown" (\n -> "date" </> mvpath ~/> n) fname
+      mvto = mv </> name
+  in do createDirectoryIfMissing True mv
+        moveFile (q ^. _FoscamDirectory </> name) mvto
+        mapM_ (\n -> mapM_
+                (\k -> 
+                  let out = base </> "link" </> k ~/> n
+                  in do createDirectoryIfMissing True out
+                        createSymbolicLinkIfMissing mvto (out </> name)) lnpaths) fname
+     
+moveFile ::
   FilePath
   -> FilePath
   -> IO ()
-copyFileIf fr to =
-  do e <- doesFileExist fr
-     when e (do p <- getPermissions fr
-                when (readable p) (copyFile fr to))
+moveFile f t =
+  do e <- doesFileExist f
+     when e (do p <- getPermissions f
+                when (readable p) (renameFile f t))
 
 createSymbolicLinkIfMissing ::
   FilePath
@@ -52,36 +66,4 @@ createSymbolicLinkIfMissing ::
   -> IO ()
 createSymbolicLinkIfMissing file1 file2 =
         do s <- doesFileExist file2
-           print (s, file2)
            unless s (createSymbolicLink file1 file2)
-
-run ::
-  [FilenamePath]
-  -> FilePath
-  -> FilePath
-  -> IO ()
-run p d e =
-  undefined {-}
-  do c <- getFoscamDirectoryContents d
-     mapM_ (\w -> links w p d e) (filenames c)
--}
--- move :: 
---   FilePath [directory containing file]
---   Filename [filename in directory]
---   (FilePath, FilenamePath) [move to]
---   (FilePath, [FilenamePath]) [link to]
-links ::
-  Filename
-  -> [FilenamePath]
-  -> FilePath
-  -> FilePath
-  -> IO ()
-links c q d e =
-  let name = getFilename c
-      createSymbolicLinkIfMissing file1 file2 =
-        do s <- doesFileExist file2
-           unless s (createSymbolicLink file1 file2)
-  in mapM_ (\p -> let outdir = e </> p ~/> c
-                  in do createDirectoryIfMissing True outdir
-                        createSymbolicLinkIfMissing (d </> name) (outdir </> name)) q
-
---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+module Main(
+  main
+) where
+
+import Data.Foscam.Sort(foscamsort)
+import System.IO(IO)
+
+main ::
+  IO ()
+main =
+  foscamsort
